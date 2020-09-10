0f31e4ee738de5d89ea5246e0ff6069dd27bc77a
Jess Tremper <jess@voidwell.com>
2020-09-10 19:58:19 -0400

    Init commit

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+apt install -y libprotoc-dev libprotobuf-dev protobuf-compiler
+
+export GOPATH=$HOME/go
+export GOROOT=/usr/local/go
+export PATH=$PATH:/$GOROOT/bin:/$GOPATH/bin
+
+go get -u google.golang.org/grpc
+go get -u github.com/golang/protobuf/protoc-gen-go
+
+find . -name '*.proto' -type f | while read line; do
+    protoc -I $(dirname "$line") $line --go_out=plugins=grpc:$(basename "${line%.*}")
+done
